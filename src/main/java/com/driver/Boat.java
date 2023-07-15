de042afeceb89608684afe04146fa320bdc6a6b2
Rajat <rs.sunny100@gmail.com>
2023-07-15 19:00:13 +0530

    done

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.driver;
+
+/**
+ *
+ * @author rssun
+ */
+public class Boat implements WaterVehicle {
+    
+    private String name;
+    private int capacity;
+    
+    public Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
+   
+    public String getVehicleName(){
+        return this.name;
+    }
+    public int getVehicleCapacity(){
+        return this.capacity;
+    }
+    
+}
--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -8,18 +8,56 @@ public class Car extends Vehicle {
     private boolean isManual;
     private int currentGear;
     private int seats;
+    
+    public Car(String name, boolean isManual){
+        super(name);
+        this.isManual = isManual;
+    }
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
+        super(name);
+        this.wheels = wheels;
+        this.type = type;
+       // this.currentGear = 1; //check again
+        this.seats = seats;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        changeGear(1);
     }
+    
+    public String getType(){
+        return this.type;
+    }
+    public int getDoors(){
+        return this.doors;
+    }
+    public int getWheels(){
+        return this.wheels;
+    }
+    public int getGears(){
+        return this.gears;
+    }
+    public int geturrentGear(){
+        return this.currentGear;
+    }
+    public int getgetSeats(){
+        return this.seats;
+    }
+    public boolean isManual(){
+        return this.isManual;
+    }
+    
 
     public void changeGear(int newGear){
+        this.currentGear = newGear;
 
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-
+        move(newSpeed, newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
